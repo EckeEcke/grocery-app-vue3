297161227e19dc9a7c569b0bc5665be7053790ab
Christian Eckardt <christian.eckardt@tchibo.de>
2025-01-31 14:53:34 +0100

    fix typo in client

--- api/dbClient.js
+++ api/dbClient.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const uri = `mongodb+srv://ceckardt254:${process.env.DATABASE_PASSWORD}@cluster0.sent83.mongodb.net/?retryWrites=true&w-majority&appName=Cluster0`
+const uri = `mongodb+srv://ceckardt254:${process.env.DATABASE_PASSWORD}@cluster0.sent83.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 let client
 
 export const connectToDatabase = async () => {
